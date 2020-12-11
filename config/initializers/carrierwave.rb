f2a8ca0fd0d7ec661213e0c0dfae4afc04777311
Julien Bouland <bouland.julien@gmail.com>
2020-12-11 14:51:55 +0100

    style and liks to dashboard

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,11 +7,11 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required unless using use_iam_profile
     use_iam_profile:       true,                         # optional, defaults to false
     region: 'eu-west-3',
-    path_style: :true
+    # path_style: :true
   }
   config.storage = :fog
   config.fog_directory  = ENV['FOG_DIRECTORY']                                     # required
-  config.fog_public     = false                                                 # optional, defaults to true
+  # config.fog_public     = false                                                 # optional, defaults to true
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
   # For an application which utilizes multiple servers but does not need caches persisted across requests,
   # uncomment the line :file instead of the default :storage.  Otherwise, it will use AWS as the temp cache store.
