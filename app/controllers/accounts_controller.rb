382ba9cb321bfed73b5e99f2263501a4ce3423fb
Julien Bouland <bouland.julien@gmail.com>
2020-12-14 15:29:48 +0100

    conditional statement for follow_accout

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -15,7 +15,12 @@ class AccountsController < ApplicationController
 
   def follow_account
     account_id = params[:follow_id]
-    Follower.create(follower: current_account.id, following: follower_id)
+    if Follower.create(follower_id: current_account.id, following_id: follower_id)
+      flash[:success] = "Now following user"
+    else
+      flash[:danger] = "Unable to follow user"
+    end
+    redirect_to dashboard_path
   end
 
   private
--- app/models/concerns/follower.rb
+++ app/models/concerns/follower.rb
@@ -1,5 +1,5 @@
 class Follower < ApplicationRecord
-  belongs_to :accounts
+  # belongs_to :accounts
   validates_uniqueness_of :follower_id, scope: :following_id
 
 end
