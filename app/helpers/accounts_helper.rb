cb3f5dc149eebceef7d3548d58487b0bb3d7ae7a
Julien Bouland <bouland.julien@gmail.com>
2020-12-14 15:34:15 +0100

    removed thumb on pics

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,6 +1,6 @@
 module AccountsHelper
   def profile_picture account, width = 100
-    image_path = account.image.present? ? account.image.thumb.url :  "placeholder-profile.jpg"
+    image_path = account.image.present? ? account.image.url :  "placeholder-profile.jpg"
     image_tag(image_path, width: width, class: "img-circle")
   end
 
