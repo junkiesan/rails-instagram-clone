d4089afe9efcbb95d91832e9577efcd5fdd9b3f4
Julien Bouland <bouland.julien@gmail.com>
2020-12-14 15:41:08 +0100

    seeds: new users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Account.create(username: "julien", email: "lol@example.com", password: "123456")
+Account.create([
+  { username: "jujulien", first_name: "Julien", last_name: "Bouland", email: "lol@example.com", password: "123456" },
+  { username: "lauuura", first_name: "Laura", last_name: "Schmidt", email: "laura@example.com", password: "123456" },
+  { username: "alexXx", first_name: "Alex", last_name: "Young", email: "alex@example.com", password: "123456" },
+])
