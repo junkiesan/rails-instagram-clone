347cb12d7a829a9601513c4b56b78ec731fb23b2
Julien Bouland <bouland.julien@gmail.com>
2020-12-15 16:20:20 +0100

    focus added on comment

--- app/javascript/posts.js
+++ app/javascript/posts.js
@@ -1,5 +1,10 @@
 $(function(){
   // jquery loaded 
+  $("#post-comment").on("click", function(){
+    $("#comment_comment").focus();
+
+  });
+
   $(".post-like").on("click", function(){
     var post_id = $(this).data("id");
     
