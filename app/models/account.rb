a9fef1b85bd584e95b33e47252ee664a0ec6f9e3
Julien Bouland <bouland.julien@gmail.com>
2020-12-16 14:59:25 +0100

    fb auth done

--- app/models/account.rb
+++ app/models/account.rb
@@ -21,4 +21,17 @@ class Account < ApplicationRecord
   def total_following
     Follower.where(following_id: self.id).count
   end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.first_name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+      user.username = auth.info.name.gsub(" ", "")
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
 end
