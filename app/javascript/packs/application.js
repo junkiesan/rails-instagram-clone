dab2b456a7d5fc8917d0b73db14a34c822bc5e06
Julien Bouland <bouland.julien@gmail.com>
2020-12-17 12:54:36 +0100

    jquery fixed/slim not working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,22 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(function(){
+  // jquery loaded 
+  $("#post-comment").on("click", function(){
+    $("#comment_comment").focus();
+
+  });
+
+  $(".post-like").on("click", function(){
+    var post_id = $(this).data("id");
+    
+    $.ajax({
+      url: "/post/like/"+post_id,
+      method: "GET"
+    }).done(function(response){
+        console.log(response);
+      })
+  }) 
+});
